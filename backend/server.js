8a5d2bdc0c9a0d67af0587750cb0d6fcb3820b80
hkhx417 <hkhx417@students.cs.ubc.ca>
2020-06-12 23:23:23 -0700

    Backend text similarity API

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,6 @@ app.get('/', function(req, res) {
 app.post("/users/add", (req, res) => {
     const {id, name, email, location, introduction, skills, ideas} = req.body;
 
-    console.log(id);
-
     const newUser = new users({
         id,
         name,
@@ -49,6 +47,33 @@ app.post("/users/add", (req, res) => {
         .save()
         .then(() => res.json("New user is added!"))
         .catch((err) => res.status(400).json("Error: " + err));
+});
+
+app.get("/users/:id", (req, res) => {
+    users.findById(req.params.id)
+        .then((user) => res.json(user))
+        .catch((err) => res.status(400).json("Error: " + err));
+});
+
+const options = {
+    method: 'GET',
+    url: 'https://twinword-text-similarity-v1.p.rapidapi.com/similarity/',
+    qs: {
+        text1: 'The hippocampus is a major component of the brains of humans and other vertebrates. It belongs to the limbic system and plays important roles in the consolidation of information from short-term memory to long-term memory and spatial navigation. Humans and other mammals have two hippocampi%2C one in each side of the brain. The hippocampus is a part of the cerebral cortex%3B and in primates it is located in the medial temporal lobe%2C underneath the cortical surface. It contains two main interlocking parts%3A Ammon\'s horn and the dentate gyrus.',
+        text2: 'An important part of the brains of humans and other vertebrates is the hippocampus. It\'s part of the limbic system and moves information from short-term to long-term memory. It also helps us move around. Humans and other mammals have two hippocampi%2C one on each side. The hippocampus is a part of the cerebral cortex%3B and in primates it is found in the medial temporal lobe%2C beneathe the cortical surface. It has two main interlocking parts%3A Ammon\'s horn and the dentate gyrus.'
+    },
+    headers: {
+        'x-rapidapi-host': 'twinword-text-similarity-v1.p.rapidapi.com',
+        'x-rapidapi-key': 'f3512dba28msha0bfafff623f90fp18ccabjsn78a20a660e54',
+        useQueryString: true
+    }
+};
+
+app.get("/matching", (req, res) => {
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(body);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
