268407fa53686f68cd08d4cfd4eada6c1aa4cfcc
hkhx417 <hkhx417@students.cs.ubc.ca>
2020-06-12 21:56:46 -0700

    Backend set up

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,30 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 
+// MongoDB configuration
+const userProfile = require("./models/users.model");
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connection established successfully");
+});
+
 
 app.get('/', function(req, res) {
     res.send("server success");
 });
+
+
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
