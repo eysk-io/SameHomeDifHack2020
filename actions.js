625f04bb1354e0a2dfa0f4dfe50a90ac22c88db5
hkhx417 <hkhx417@students.cs.ubc.ca>
2020-06-13 22:33:25 -0700

    Added functions for used in the front end

---
+++ actions.js
@@ -0,0 +1,52 @@
+// let url = "https://XXXXXXX.herokuapp.com";
+let url = "http://localhost:5000";
+
+
+// selection = 1 for select and selection = -1 for deselect
+const selectMatching = (currentid, targetid, selection) => {
+
+    fetch(url + "/user/select", {
+        method: 'POST',
+        body: {userid: currentid, targetid: targetid, selection: selection}
+    })
+        .then(res => res.json())
+        .then(user => {
+            console.log(user);
+            // if user["matching"]["targetid"]["score"] > 10   we have a match
+
+        }).catch(err => console.log(err));
+};
+
+const addNewUser = (id, name, email, location, introduction, skills, ideas, matching, displayName, picture, headline, interests) => {
+
+    //use any number of fields as appropriate
+    fetch(url + "/users/add", {
+        method: 'POST',
+        body: {id: id, name: name, email: email, location: location, introduction: introduction, skills: skills, ideas: ideas, matching: matching, displayName:displayName, picture: picture, headline: headline, interests: interests}
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+        }).catch(err => console.log(err));
+};
+
+
+getUser = (id) => {
+    fetch( url + "/users/?id=" + id, {
+        method: 'GET',
+    })
+        .then(res => res.json())
+        .then(user => {
+            console.log(user);
+        }).catch(err => console.log(err));
+};
+
+getMatching = (id) => {
+    fetch( url + "/users/matching/?id=" + id, {
+        method: 'GET',
+    })
+        .then(res => res.json())
+        .then(matching => {
+            console.log(matching);
+        }).catch(err => console.log(err));
+};
