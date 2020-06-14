9710a46561fcf587119986123f395796bba13e1f
Eric Kim <erickim195@gmail.com>
2020-06-13 17:12:17 -0700

    early version of dragging functionality

--- client/screens/matching/MatchProfileModal.js
+++ client/screens/matching/MatchProfileModal.js
@@ -1,14 +1,43 @@
-import React, { useState } from 'react';
-import { Text, SafeAreaView } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React from 'react';
+import { Text, SafeAreaView, View, StyleSheet, Image } from 'react-native';
 
 const MatchProfileModal = ({ img, name, bio, skills, matched }) => {
     return (
-        <SafeAreaView>
-            <Text>{name}</Text>
-            {/* <Text>MatchProfileModal</Text> */}
+        <SafeAreaView style={styles.container}>
+            <Image
+                style={styles.img}
+                source={img}
+            />
+            <View style={styles.text}>
+                <Text style={styles.name}>{name}</Text>
+                <Text>{bio}</Text>
+                <Text>{skills}</Text>
+                <Text>{matched.toString()}</Text>
+            </View>
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        flex: 1
+    },
+    text: {
+        margin: 10,
+    },
+    name: {
+        fontSize: 40,
+    },
+    img: {
+        marginTop: 35,
+        marginBottom: 35,
+        width: 200,
+        height: 200,
+        borderRadius: 200,
+        borderWidth: 8,
+        borderColor: 'black'
+    }
+})
 
 export default MatchProfileModal;
--- client/screens/matching/MatchingPage.js
+++ client/screens/matching/MatchingPage.js
@@ -1,11 +1,35 @@
-import React, { useState } from 'react';
-import { Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useState, useCallback, useRef } from 'react';
+import { SafeAreaView, TouchableOpacity, StyleSheet, View, PanResponder, Animated, Dimensions } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCheckCircle, faTimesCircle, faUserCircle } from '@fortawesome/free-regular-svg-icons';
 import MatchProfileModal from './MatchProfileModal';
 import PROSPECTS from './data/prospects';
 
-const MatchingPage = () => {
+const MatchingPage = ({ navigation }) => {
     const [index, setIndex] = useState(0);
     const [matchedProspects, setMatchedProspects] = useState(PROSPECTS);
+
+    const position = new Animated.ValueXY();
+    const { height, width } = Dimensions.get('window');
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (evt, gestureState) => {
+            position.setValue({
+                x: gestureState.dx,
+                y: gestureState.dy
+            });
+        },
+        onPanResponderRelease: (evt, gestureState) => {
+            if (position.x._value < 0) {
+                handleDisLike();
+                console.log(position.x._value)
+            } else if (position.x._value > 0) {
+                handleLike();
+                console.log(position.x._value)
+            }
+        }
+    });
 
     function handleLike() {
         if (matchedProspects[index]) {
@@ -25,32 +49,67 @@ const MatchingPage = () => {
         return i;
     }
 
-    function getProspect() {
-        let result = ["No matches available"];
-        if (matchedProspects && matchedProspects[index]) {
-            result = matchedProspects;
-        }
-        return result;
-    }
+    const navigateHome = useCallback(() => {
+        navigation.navigate('Home');
+    });
 
     return (
-        <SafeAreaView>
-            <Text>MatchingPage</Text>
-            <MatchProfileModal
-                name={matchedProspects[index].name}
-                img={matchedProspects[index].img}
-                bio={matchedProspects[index].bio}
-                skills={matchedProspects[index].skills}
-                matched={matchedProspects[index].matched}
-            />
-            <TouchableOpacity onPress={handleLike}>
-                <Text>Like button</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={handleDisLike}>
-                <Text>DisLike button</Text>
-            </TouchableOpacity>
-        </SafeAreaView>
+        <SafeAreaView style={styles.container}>
+            <Animated.View style={
+                [
+                    {
+                        transform: [
+                            { translateX: position.x },
+                            { translateY: position.y }
+                        ]
+                    },
+                    styles.profile
+                ]
+            }
+                {...panResponder.panHandlers}
+            >
+                <MatchProfileModal
+                    name={matchedProspects[index].name}
+                    img={matchedProspects[index].img}
+                    bio={matchedProspects[index].bio}
+                    skills={matchedProspects[index].skills}
+                    matched={matchedProspects[index].matched}
+                />
+            </Animated.View>
+            <View horizontal={true} style={styles.buttons}>
+                <TouchableOpacity onPress={handleDisLike}>
+                    <FontAwesomeIcon size={70} style={styles.button} icon={faTimesCircle} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={navigateHome}>
+                    <FontAwesomeIcon size={70} style={styles.button} icon={faUserCircle} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={handleLike}>
+                    <FontAwesomeIcon size={70} style={styles.button} icon={faCheckCircle} />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView >
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    profile: {
+        alignItems: 'center',
+        width: '85%',
+        height: '80%',
+        borderWidth: 1,
+        borderRadius: 25,
+    },
+    buttons: {
+        flexDirection: 'row'
+    },
+    button: {
+        margin: 20
+    }
+})
 
 export default MatchingPage;
